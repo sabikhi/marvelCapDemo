1b2958cd90ad2d4b75b4f8e73f64fdc227a57a8c
Rohit Sabikhi <sabikhi@gmail.com>
2024-01-25 20:56:56 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // config.js
 const baseURL = 'https://gateway.marvel.com/v1/public/';
 const limit = 100;
-const publicKey = '71156a2ff155a6cbf3fa5813c8d17f01'; // Replace with your actual public key
-const privateKey = '19efff1a2007832e2bd7f677306b831df0eb5243'; // Replace with your actual private key
+const publicKey = '<PUBLIC KEY FROM MARVEL>'; // Replace with your actual public key
+const privateKey = '<PRIVATE KEY FROM MARVEL>'; // Replace with your actual private key
 
 const CHARACTERS_ENDPOINT = 'characters';
 const COMICS_ENDPOINT = 'comics';
